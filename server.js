c77adfcfcdeaea0c3115e97fe122197e0404900d
realzlight <legitzlight@gmail.com>
2026-06-22 08:53:22 +0530

    Added Vouce Chag Features Removed Bugs

--- server.js
+++ server.js
@@ -173,5 +173,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Void running on http://localhost:${PORT}`);
+  console.log(`yooo Void running on http://localhost:${PORT}`);
 });
